607921f73938ff271fb0e10e6e85b3a86607229f
Rushikesh0020 <rushikeshpatil0020@gmail.com>
2021-03-03 15:12:19 +0530

    Burger Builder App Version 1.0

---
+++ src/Components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import Aux from '../../../hoc/Auxiliary/Auxiliary';
+import Button from '../../UI/Button/Button';
+
+class OrderSummary extends Component {
+    //This could be a functional component!, Doesn't have to be a class based. 
+    componentDidUpdate(){
+        console.log('[OrderSummary] Updated!');
+    }
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients).map(igKey => {
+            return (
+                <li key={igKey}>
+                    <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {this.props.ingredients[igKey]}
+                </li>);
+        });
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients: </p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to Checkout!?</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchasedContinued}>CONTINUE</Button>
+            </Aux>
+        );
+    }
+}
+export default OrderSummary;
